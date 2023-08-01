c0431424b61ea7e5ee2aaccbf38b30977f353f60
wm <nazir.kuchmenov@gmail.com>
2023-08-01 17:09:43 +0300

    hw2

--- src/channel.py
+++ src/channel.py
@@ -13,7 +13,7 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
@@ -25,12 +25,12 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(channel)
 
     def to_json(self, filename):
         data = {
-            "channel_id": self.channel_id,
+            "channel_id": self.__channel_id,
             "title": self.title,
             "description": self.description,
             "url": self.url,
@@ -45,3 +45,8 @@ class Channel:
     @classmethod
     def get_service(cls):
         return cls.youtube
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
