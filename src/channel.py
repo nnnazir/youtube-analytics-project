da34f123ea4123fd1ce4d7bf1914d48dcb431139
wm <nazir.kuchmenov@gmail.com>
2023-08-01 16:31:12 +0300

    hw2

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,5 @@
+import json
 import os
-
 from googleapiclient.discovery import build
 
 
@@ -14,6 +14,13 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.url = f"https://www.youtube.com/channel/{channel_id}"
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.subscribers_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -21,3 +28,20 @@ class Channel:
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(channel)
 
+    def to_json(self, filename):
+        data = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscribers_count": self.subscribers_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
